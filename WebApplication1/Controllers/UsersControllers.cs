9a039426641cca80cda972ed2186f8646cbe4f0e
YasinZabun <ysnzbnsvs@gmail.com>
2021-03-20 15:40:18 +0300

    Add project files.

---
+++ WebApplication1/Controllers/UsersControllers.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersControllers : ControllerBase
+    {
+        private List<User> _user = Datas.Users();
+        
+        public User yeni(int id)
+        {
+            User usr = _user.FirstOrDefault(x => x.Id == id);
+            return usr;
+        }
+        [HttpGet]
+        public List<User> Get()
+        {
+            return _user;
+        }
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            User usr = _user.FirstOrDefault(x => x.Id == id);
+            return usr;
+        }
+        [HttpPost]
+        public User Post([FromBody]User user)
+        {
+            _user.Add(user);
+            return user;
+        }
+        [HttpPut]
+        public User Put([FromBody]User user)
+        {
+            User edituser = _user.FirstOrDefault(x => x.Id == user.Id);
+            edituser.FirstName = "ben güncellendim";
+            return edituser;
+        }
+        [HttpDelete("{id}")]
+        public User Delete(int id)
+        {
+            User user = yeni(id);
+            _user.Remove(user);
+            return user;
+        }
+        
+
+    }
+}
---
+++ WebApplication1/Data/Datas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public static class Datas
+    {
+        private static List<User> _users;
+                
+        public static List<User> Users() {
+             if (_users == null)
+             {
+                _users = new List<User>();
+                _users.Add(new User { Id = 1, FirstName = "yasin", LastName = "zabun" });
+                _users.Add(new User { Id = 2, FirstName = "ali", LastName = "mazak" });
+                _users.Add(new User { Id = 3, FirstName = "veli", LastName = "basdemir" });
+                _users.Add(new User { Id = 4, FirstName = "kenan", LastName = "bas" });
+                _users.Add(new User { Id = 5, FirstName = "hakan", LastName = "yerden" });
+                _users.Add(new User { Id = 6, FirstName = "serkan", LastName = "gökten" });
+            }
+            return _users;
+        }
+
+    }
+}
